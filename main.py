8527999aa02930266f8da447c72e5a33b1c82f69
AdeebNqo <adeebnqo@gmail.com>
2014-03-03 21:52:00 +0200

    can get doc into dict

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import pysolr
 #
 # Method for removing nests in xml
 #
+
+#solr object
+solr = pysolr.Solr('http://localhost:8983/solr/')
+
 record_cache = []
 def cleanrecord(record):
 	children = record.getchildren()
@@ -13,19 +17,23 @@ def cleanrecord(record):
 			record_cache.append(item)
 		else:
 			cleanrecord(item)
-def indexDoc(doc):
-	print('indexing doc...')
-	print(doc)
+def indexrecords(docs):
+	print('indexing docs...')
+	print(docs)
+	#solr.add(docs)
 afile = sys.argv[1]
 xmltree = etree.parse(afile)
 root = xmltree.getroot()
 listRecords = root.getchildren()[2]
 records = listRecords.getchildren()
+
+actualrecords = []
 for record in records:
 	cleanrecord(record)
-	doc = ''
+	doc = {}
 	for item in record_cache:
-		doc=doc+'\''+item.tag.split('}')[1] +'\' : \''+item.text+'\',\n'
-	indexDoc(doc)#index doc
+		doc[item.tag.split('}')[1]] = item.text
+	actualrecords.append(doc)
 	record_cache = [] #clearing record cache
 	break
+indexrecords(actualrecords)
